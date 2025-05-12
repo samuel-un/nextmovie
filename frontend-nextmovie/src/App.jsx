454ec8ce2e74fb5432e402cb40adfd4500770756
samuel-un <samuelun00@gmail.com>
2025-05-12 12:32:43 +0200

    Login component with functional implementations.

--- frontend-nextmovie/src/App.jsx
+++ frontend-nextmovie/src/App.jsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
+
 function App() {
-	return <RegisterForm />;
+	return (
+		<Routes>
+			<Route path="/login" element={<LoginForm />} />
+			<Route path="/register" element={<RegisterForm />} />
+		</Routes>
+	);
 }
+
 export default App;
