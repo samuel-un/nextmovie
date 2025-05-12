f1a45f24fe6862001b0a7fd334e44253a56dc341
samuel-un <samuelun00@gmail.com>
2025-05-12 12:42:22 +0200

    merge backend fusion

--- backend-nextmovie/app/Http/Controllers/Api/UserController.php
+++ backend-nextmovie/app/Http/Controllers/Api/UserController.php
@@ -8,28 +8,38 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index()
-    {
-        return User::all();
-    }
+	public function index()
+	{
+		$users = User::all();
+		return response()->json($users, 200);
+	}
 
-    public function show($id)
-    {
-        return User::findOrFail($id);
-    }
+	public function show($id)
+	{
+		$user = User::findOrFail($id);
+		return response()->json($user, 200);
+	}
 
-    public function store(Request $request)
-    {
-        $user = User::create($request->all());
-        return response()->json($user, 201);
-    }
+	public function store(Request $request)
+	{
+		$data = $request->all();
+		$data['password'] = bcrypt($data['password']);
+		$user = User::create($data);
+		return response()->json($user, 201);
+	}
+
 
     public function update(Request $request, $id)
-    {
-        $user = User::findOrFail($id);
-        $user->update($request->all());
-        return response()->json($user, 200);
-    }
+	{
+		$user = User::findOrFail($id);
+		$data = $request->all();
+		if (isset($data['password'])) {
+			$data['password'] = bcrypt($data['password']);
+		}
+		$user->update($data);
+		return response()->json($user, 200);
+	}
+
 
     public function destroy($id)
     {
